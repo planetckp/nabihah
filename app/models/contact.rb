b6f49300f146b84236a7f12ffb4be9f3a128d888
Parwej <planetckp@gmail.com>
2017-05-07 00:32:56 +0530

    added mobile to sendgrid

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :mobile,    :validate => true
   attribute :message,	:validate => true
   attribute :nickname,  :captcha  => true
 
